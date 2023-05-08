3a5ac2ed6b459104299634daad84984e4f9fb28c
Rubén Chiquin <randreu28@gmail.com>
2023-05-08 12:51:29 +0200

    Makes a better loading UI

--- src/app/artwork/[id]/loading.tsx
+++ src/app/artwork/[id]/loading.tsx
@@ -1,3 +1,10 @@
+import { CubeIcon } from "@heroicons/react/24/outline";
+
 export default function Loading() {
-  return <div className="">Loading...</div>;
+  return (
+    <div className="space-y-5">
+      <h1 className="text-center text-3xl">Loading</h1>
+      <CubeIcon className="h-10 w-10 text-teal-500 animate-spin mx-auto" />
+    </div>
+  );
 }
